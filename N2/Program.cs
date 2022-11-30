b22ae2120c431c8af9e2e3a4d8601be0c333d22d
lolmem745 <93839006+lolmem745@users.noreply.github.com>
2022-11-30 21:45:52 +0300

    Second problem solved

--- N2/Program.cs
+++ N2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+void SumOfElements(int m, int n)
+{
+    result += Math.Min(m, n);
+    if (Math.Max(m, n) != Math.Min(m, n)) SumOfElements(Math.Min(m, n) + 1, Math.Max(m, n));
+}
+SumOfElements(m, n);
+Console.Write($"{result}");
