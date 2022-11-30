590cd18c3c07245a4670732f2d1549e7e326cf59
lolmem745 <93839006+lolmem745@users.noreply.github.com>
2022-11-30 21:34:04 +0300

    Third problem solved

--- N3/Program.cs
+++ N3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int AckermannFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 & n == 0) return AckermannFunction(m - 1, 1);
+    else return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+}
+
+Console.Write("Вычисление значения функции Аккермана\nВведите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int i = AckermannFunction(m, n);
+Console.WriteLine($"A({m},{n}) = {i}");
